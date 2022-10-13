349a03f1d177fc8bddf68b95e120221e6503b493
Florin Brasoveanu <gflorinb@yahoo.com>
2022-10-13 10:26:06 +0300

    20221013

--- app/src/main/java/my/logon/screen/screens/CreareComanda.java
+++ app/src/main/java/my/logon/screen/screens/CreareComanda.java
@@ -1870,7 +1870,7 @@ public class CreareComanda extends Activity implements AsyncTaskListener, Valoar
             dateArticol.setProductCode(artCmd.getCodArticol());
             dateArticol.setQuantity(artCmd.getCantitate());
             dateArticol.setUnit(artCmd.getUm());
-            dateArticol.setValPoz((artCmd.getPretUnit() / artCmd.getMultiplu()) * artCmd.getCantitate());
+            dateArticol.setValPoz(artCmd.getPret());
 
             if (artCmd.getArticolMathaus() != null)
                 dateArticol.setTip2(artCmd.getArticolMathaus().getTip2());
